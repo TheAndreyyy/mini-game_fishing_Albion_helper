aa951a93a40d13e0388fc25b6b1f8f9f39f95e44
TheAndreyyy <theandreyyy@gmail.com>
2021-01-02 18:44:27 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace mini_game_fishing_Albion_helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(93, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Стоп";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 15;
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 127);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Помощник по мини-игре \"рыбалка\" в Альбион";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;//for working mouse and buttons
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace mini_game_fishing_Albion_helper
+{
+
+    public partial class Form1 : Form
+    {
+        #region options_of_program
+        Stopwatch stopwatch = new Stopwatch();//debug for count
+        int state_of_fishing = -1;//состояние процесса рыбалки
+        /*
+         * 2 - мини-игра
+         */
+        Point pos_of_match = new Point(0, 0);
+        bool mouse_pressed_now = false;
+        bool prev_state_fishing = false;
+        #endregion
+
+        #region OPENCV_options
+
+        //string path_for_files = "C:\\Users\\Andrey\\Desktop\\";
+        string path = "D:\\";
+
+        #endregion
+
+        #region mouse_options
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        #endregion
+
+        #region try5_find_matches_ITS_FUCKING_WORKING!
+        public void Try5(string where, string what, float percent)
+        {
+            // Запускаем внутренний таймер объекта Stopwatch
+            stopwatch.Start();
+            Image<Bgr, byte> source = new Image<Bgr, byte>(path + where + ".bmp"); // Image A где ищем, картинка рыбалки
+            Image<Bgr, byte> template = new Image<Bgr, byte>(path + what + ".bmp"); // Image B что ищем
+            Image<Bgr, byte> imageToShow = source.Copy();
+            using (Image<Gray, float> result = source.MatchTemplate(template, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed))
+            {
+                double[] minValues, maxValues;
+                Point[] minLocations, maxLocations;
+                result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+
+                // You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
+                if (maxValues[0] > percent)//0.9
+                {
+                    // This is a match. Do something with it, for example draw a rectangle around it.
+                    Rectangle match = new Rectangle(maxLocations[0], template.Size);
+                    imageToShow.Draw(match, new Bgr(Color.Red), 3);
+                    pos_of_match = maxLocations[0];
+                    label4.Text = "Позиция совпадения: X - " + pos_of_match.X + " | Y - " + pos_of_match.Y;
+                }
+                else
+                {
+                    if (what != "fish_is_mine")
+                    {
+                        label4.Text = "Совпадений не найдено";
+                    }
+                }
+                label3.Text = "Процент совпадения: " + Math.Round(maxValues[0], 3).ToString();
+            }
+            imageToShow.Save(path + "final.bmp");
+            stopwatch.Stop();// Останавливаем внутренний таймер объекта Stopwatch
+            label5.Text = "Время опознания: " + (stopwatch.ElapsedMilliseconds).ToString() + " мс";//time of exec CV
+            stopwatch.Reset();
+        }
+        #endregion
+
+        public void MyMouseMove(int x, int y)
+        {
+            PointConverter pc = new PointConverter();
+            System.Drawing.Point pt = new System.Drawing.Point(x, y);
+            Cursor.Position = pt;
+        }
+
+        public Form1()
+        {
+            TopMost = true;
+            InitializeComponent();
+        }
+
+        public void Mouse_press(int state)
+        {
+            if (state == 0)
+            {
+                //Call the imported function with the cursor's current position
+                uint X = (uint)Cursor.Position.X;
+                uint Y = (uint)Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
+                //Thread.Sleep(time); 
+                //mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+                mouse_pressed_now = true;
+            }
+            else
+            if (state == 1)
+            {
+                //Call the imported function with the cursor's current position
+                uint X = (uint)Cursor.Position.X;
+                uint Y = (uint)Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+                mouse_pressed_now = false;
+            }
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = "Позиция курсора: " + Cursor.Position.X + " | " + Cursor.Position.Y;
+        }
+
+        private void Get_screenshot(int x1, int y1, int x2, int y2)
+        {
+            Bitmap printscreen = new Bitmap(x2 - x1, y2 - y1/*Screen.PrimaryScreen.Bounds.Height*/);
+
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+
+            graphics.CopyFromScreen(x1, y1, 0, 0, printscreen.Size);
+
+            printscreen.Save(path + "screenshot.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+            //Close();
+        }
+
+        private void Start_fishing()
+        {
+            //if (timer5.Enabled==false)
+            //{
+            //timer5.Enabled = true;
+            //}
+            state_of_fishing = 0;//proc of fish         start
+            button2.Enabled = false;//start             false
+            button3.Enabled = true;//stop               true
+            timer3.Enabled = true;//timer for fishing   run
+        }
+
+        private void Stop_fishing()
+        {
+            state_of_fishing = -1;//proc of fish        stop
+            button2.Enabled = true;//start              true
+            button3.Enabled = false;//stop              false
+            timer3.Enabled = false;//timer for fishing  stop
+            label6.Text = "Ждем...";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Start_fishing();
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            state_of_fishing = 2;
+            pos_of_match.X = 0;
+            pos_of_match.Y = 0;
+
+            if (state_of_fishing >= 0)
+            {
+                switch (state_of_fishing)
+                {
+                    case 2://+ мини-игра, ловим рыбу
+                        {
+                            label6.Text = "Понеслась! Рыбачим!";
+                            Get_screenshot(850, 520, 1070, 570);//скрин мини-игры адекватные = начальные + 100/170 = -120/-80 | начальный значения 750, 350, 1200, 650
+                            pos_of_match.X = 0;
+                            Try5("screenshot", "mini-game", 0.65f);
+                            if (pos_of_match.X > 20 && pos_of_match.X < 160)
+                            {
+                                Mouse_press(0);
+                                //Close();
+                                prev_state_fishing = true;
+                            }
+                            else
+                            if (pos_of_match.X > 20 && pos_of_match.X > 160)
+                            {
+                                Mouse_press(1);
+                                prev_state_fishing = true;
+                            }
+                            
+                            if (pos_of_match.X==0 && prev_state_fishing == true)
+                            {
+                                Mouse_press(1);
+                                prev_state_fishing = false;
+                            }
+                            Get_screenshot(767, 66, 1151, 88);//скрин выигрыша, поймали или нет
+                            //Close();
+                            pos_of_match.X = 0;
+                            Try5("screenshot", "fish_is_mine", 0.75f);
+                            if (pos_of_match.X > 0 && mouse_pressed_now == false)
+                            {
+                                mouse_pressed_now = true;
+                            }
+                            label6.Text = "успокоились";
+                        }
+                        break;
+                }
+                pos_of_match.X = 0;
+            }
+            else
+            {
+                Stop_fishing();
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Stop_fishing();
+        }
+    }
+}
